2e4a447310201c03fb0cc006db61dcff6def8ee3
Fv-store <fvstore123@gmail.com>
2023-11-09 15:32:10 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# INSTALL
+<pre><code>wget https://raw.githubusercontent.com/Fv-store/free/main/main.sh && chmod +x main.sh && ./main.sh</pre></code>
+
+## Auto Script Free By FV-store
+```
+- Tidak Untuk Di Perjual Belikan
+- Wajib Bawa Domain Sendiri
+- Tanpa Izin Script
+```
+
+## SUPPORT OS 
+```
+Debian 9/10
+Ubuntu 18/20 LTS
+```
---
+++ setup.sh
@@ -0,0 +1,271 @@
+#!/bin/bash
+clear
+REPO="https://raw.githubusercontent.com/Fv-store/free/main/"
+red='\e[1;31m'
+green='\e[1;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+date=$(date -R | cut -d " " -f -5)
+IP=$(wget -qO- ipinfo.io/ip);
+domain=$(cat /etc/xray/domain)
+date=$(date +"%Y-%m-%d")
+
+cd /root
+#System version number
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+
+if [[ -e /etc/debian_version ]]; then
+	source /etc/os-release
+	OS=$ID # debian or ubuntu
+elif [[ -e /etc/centos-release ]]; then
+	source /etc/os-release
+	OS=centos
+fi
+
+echo -e "[ ${green}INFO${NC} ] Preparing the install file"
+sleep 2
+apt update -y
+apt update -y
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 
+apt install -y bzip2 gzip coreutils screen curl unzip
+apt install wget -y
+apt install curl -y
+apt install ruby -y
+gem install lolcat -y
+apt dist-upgrade -y
+apt-get remove --purge ufw firewalld -y 
+apt-get remove --purge exim4 -y 
+apt install -y screen curl jq bzip2 gzip coreutils rsyslog iftop \
+ htop zip unzip net-tools sed gnupg gnupg1 \
+ bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof \
+ openssl openvpn easy-rsa fail2ban tmux \
+ stunnel4 vnstat squid3 \
+ dropbear  libsqlite3-dev \
+ socat cron bash-completion ntpdate xz-utils sudo apt-transport-https \
+ gnupg2 dnsutils lsb-release chrony
+sudo apt-get install nodejs -y
+sudo apt install -y libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd
+apt install git curl -y >/dev/null 2>&1
+apt install python -y >/dev/null 2>&1
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+
+localip=$(hostname -I | cut -d\  -f1)
+hst=( `hostname` )
+dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
+if [[ "$hst" != "$dart" ]]; then
+echo "$localip $(hostname)" >> /etc/hosts
+fi
+mkdir -p /etc/xray
+mkdir -p /etc/v2ray
+touch /etc/xray/domain
+touch /etc/v2ray/domain
+touch /etc/xray/scdomain
+touch /etc/v2ray/scdomain
+mkdir -p /var/lib/SIJA >/dev/null 2>&1
+echo "IP=" >> /var/lib/SIJA/ipvps.conf
+
+secs_to_human() {
+    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
+}
+start=$(date +%s)
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
+sleep 2
+
+echo ""
+clear
+yellow "SEBELUM MASKUIN DOMAIN WAJIB POINTING TERLEBIH DAHULU !!! "
+echo " "
+read -rp "Input ur domain : " -e pp
+    if [ -z $pp ]; then
+        echo -e "
+        Nothing input for domain!
+        Then a random domain will be created"
+    else
+        echo "$pp" > /root/scdomain
+	echo "$pp" > /etc/xray/scdomain
+	echo "$pp" > /etc/xray/domain
+	echo "$pp" > /etc/v2ray/domain
+	echo "$pp" > /root/domain
+        echo "IP=$pp" > /var/lib/SIJA/ipvps.conf
+    fi
+
+clear
+wget -q ${REPO}backup/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+
+clear
+wget -q ${REPO}ssh/ins-xray && chmod +x ins-xray && ./ins-xray
+
+clear
+wget -q ${REPO}ssh/ins-ssh && chmod +x ins-ssh && ./ins-ssh
+
+clear
+wget -q ${REPO}menu/menu.zip
+unzip menu.zip
+chmod +x menu/*
+mv menu/* /usr/local/sbin
+rm -rf menu.zip
+rm -rf menu
+
+clear
+cd /usr/local/bin
+wget -q ${REPO}ws/ws.zip
+unzip ws.zip
+chmod +x *
+rm -rf ws.zip
+
+cd /etc/systemd/system
+wget -q ${REPO}ws/service.zip
+unzip service.zip
+chmod +x *
+rm -rf service.zip
+cd
+
+clear
+/etc/init.d/vnstat restart
+wget -q https://humdi.net/vnstat/vnstat-2.6.tar.gz
+tar zxvf vnstat-2.6.tar.gz
+cd vnstat-2.6
+./configure --prefix=/usr --sysconfdir=/etc >/dev/null 2>&1 && make >/dev/null 2>&1 && make install >/dev/null 2>&1
+cd
+vnstat -u -i $NET
+sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
+chown vnstat:vnstat /var/lib/vnstat -R
+systemctl enable vnstat
+/etc/init.d/vnstat restart
+rm -f /root/vnstat-2.6.tar.gz >/dev/null 2>&1
+rm -rf /root/vnstat-2.6 >/dev/null 2>&1
+clear
+
+cat > /etc/cron.d/re_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 2 * * * root /sbin/reboot
+END
+
+cat > /etc/cron.d/xp_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+0 0 * * * root /usr/bin/xp
+END
+
+cat > /etc/cron.d/cl_otm <<-END
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+2 1 * * * root /usr/bin/clearlog
+END
+
+cat > /home/re_otm <<-END
+7
+END
+
+service cron restart >/dev/null 2>&1
+service cron reload >/dev/null 2>&1
+
+clear
+cat> /root/.profile << END
+# ~/.profile: executed by Bourne-compatible login shells.
+
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+
+mesg n || true
+clear
+menu
+END
+chmod 644 /root/.profile
+
+# // Enable & Start & Restart Websocket Service
+systemctl enable ws-dropbear.service
+systemctl enable ws-stunnel.service
+systemctl start ws-dropbear.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
+systemctl restart ws-dropbear.service
+
+if [ -f "/root/log-install.txt" ]; then
+rm /root/log-install.txt > /dev/null 2>&1
+fi
+if [ -f "/etc/afak.conf" ]; then
+rm /etc/afak.conf > /dev/null 2>&1
+fi
+if [ ! -f "/etc/log-create-user.log" ]; then
+echo "Log All Account " > /etc/log-create-user.log
+fi
+history -c
+aureb=$(cat /home/re_otm)
+b=11
+if [ $aureb -gt $b ]
+then
+gg="PM"
+else
+gg="AM"
+fi
+curl -sS ifconfig.me > /etc/myipvps
+echo "------------------------------------------------------------"
+echo ""
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH		: 22"  | tee -a log-install.txt
+echo "   - SSH Websocket	: 80, 8080, 2082" | tee -a log-install.txt
+echo "   - SSH SSL Websocket	: 443, 8443" | tee -a log-install.txt
+echo "   - Stunnel4		: 447, 777" | tee -a log-install.txt
+echo "   - Dropbear		: 109, 143" | tee -a log-install.txt
+echo "   - Badvpn		: 7100-7300" | tee -a log-install.txt
+echo "   - Nginx		: 81" | tee -a log-install.txt
+echo "   - Vmess TLS		: 443, 8443" | tee -a log-install.txt
+echo "   - Vmess None TLS	: 80, 8080, 2082" | tee -a log-install.txt
+echo "   - Vless TLS		: 443, 8443" | tee -a log-install.txt
+echo "   - Vless None TLS	: 80, 8080, 2082" | tee -a log-install.txt
+echo "   - Trojan GRPC		: 443, 8443" | tee -a log-install.txt
+echo "   - Trojan WS		: 443, 8443" | tee -a log-install.txt
+echo "   - Trojan Go		: 443, 8443" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone		: Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban		: [ON]"  | tee -a log-install.txt
+echo "   - Dflate		: [ON]"  | tee -a log-install.txt
+echo "   - IPtables		: [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot		: [ON]"  | tee -a log-install.txt
+echo "   - IPv6			: [OFF]"  | tee -a log-install.txt
+echo "   - Autoreboot On	: $aureb:00 $gg GMT +7" | tee -a log-install.txt
+echo "   - AutoKill Multi Login User" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Fully automatic script" | tee -a log-install.txt
+echo "   - VPS settings" | tee -a log-install.txt
+echo "   - Admin Control" | tee -a log-install.txt
+echo "   - Change port" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo ""
+echo "------------------------------------------------------------"
+echo ""
+echo "" | tee -a log-install.txt
+rm /root/limit >/dev/null 2>&1
+rm /root/setup.sh >/dev/null 2>&1
+rm /root/setup.sh >/dev/null 2>&1
+rm /root/ins-xray.sh >/dev/null 2>&1
+rm /root/insshws.sh >/dev/null 2>&1
+touch /root/.system 
+secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
+echo -e ""
+read -n 1 -s -r -p "Pencet [ Enter ] Untuk Reboot"
+reboot
