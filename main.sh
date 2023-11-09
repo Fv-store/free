dd4e92cfe3bf2f5b7bfbccf9f6ba5abe9e71c9ba
Fv-store <141719506+Fv-store@users.noreply.github.com>
2023-11-09 17:32:24 +0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -88,6 +88,26 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
 sleep 2
+gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+    gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
+    curl -sL "$gotop_link" -o /tmp/gotop.deb
+    dpkg -i /tmp/gotop.deb >/dev/null 2>&1
+    
+        # > Buat swap sebesar 1G
+    dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+    mkswap /swapfile
+    chown root:root /swapfile
+    chmod 0600 /swapfile >/dev/null 2>&1
+    swapon /swapfile >/dev/null 2>&1
+    sed -i '$ i\/swapfile      swap swap   defaults    0 0' /etc/fstab
+
+    # > Singkronisasi jam
+    chronyd -q 'server 0.id.pool.ntp.org iburst'
+    chronyc sourcestats -v
+    chronyc tracking -v
+    
+    wget https://raw.githubusercontent.com/FadlyNotNot/v1/main/bbr.sh &&  chmod +x bbr.sh && ./bbr.sh
+clear
 
 echo ""
 clear
@@ -155,6 +175,7 @@ systemctl enable vnstat
 /etc/init.d/vnstat status
 rm -f /root/vnstat-2.6.tar.gz
 rm -rf /root/vnstat-2.6
+
 clear
 
 cat > /etc/cron.d/re_otm <<-END
