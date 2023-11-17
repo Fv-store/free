00be2d60d43f981cf278e4947b9151bf034f9f4b
Fv-store <141719506+Fv-store@users.noreply.github.com>
2023-11-17 19:21:33 +0700

    Add files via upload

--- fix/zhee-dropbear.sh
+++ fix/zhee-dropbear.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 apt -y remove dropbear
 sleep 0.5
 apt -y purge dropbear
@@ -5,6 +6,15 @@ sleep 0.5
 apt-get -y --purge remove dropbear
 sleep 0.5
 apt -y install dropbear
-wget -O /etc/default/dropbear https://raw.githubusercontent.com/Rerechan02/Example/main/fix/dropbear
+echo "=== Install Dropbear ==="
+# install dropbear
+apt -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109 -p 110 -p 69"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
 sleep 0.5
-/etc/init.d/dropbear restart￼Enter
+menu
